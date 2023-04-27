f625dad414b26b6d3d071b83feb495973fab9b7c
Kjartan Thor Wikfeldt <wikfeldt@users.noreply.github.com>
2023-04-27 12:09:21 +0200

    plausible needs lowercase

--- content/conf.py
+++ content/conf.py
@@ -131,6 +131,6 @@ def setup(app):
 import os
 if os.environ.get('GITHUB_REF', '') == 'refs/heads/main':
     html_js_files = [
-        ('https://plausible.io/js/script.js', {"data-domain": "enccs.github.io/AMD-ROCm-development", "defer": "defer"}),
+        ('https://plausible.io/js/script.js', {"data-domain": "enccs.github.io/amd-rocm-development", "defer": "defer"}),
     ]
         
